967448368366d32a4f03d9502566f6550101c486
Apoorva-Tapakire <72929437+Apoorva-Tapakire@users.noreply.github.com>
2021-11-17 17:17:02 +0530

    ProductModel

---
+++ ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectWebApplication.Models
+{
+    public class ProductModel
+    {
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ActualPrice { get; set; }
+        public int Discount { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+
+        public Byte[] Image { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
