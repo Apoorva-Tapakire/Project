e6fc25d7ebc5afcc4e27442ae4488747bcf6052e
Apoorva-Tapakire <72929437+Apoorva-Tapakire@users.noreply.github.com>
2021-11-17 12:49:13 +0530

    Controllers

---
+++ CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BalLibrary;
+using DalLibrary;
+using System.Web.Http.Cors;
+using ProjectWebApplication.Models;
+
+
+namespace ProjectWebApplication.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CustomerController : ApiController
+    {
+        public string Get()
+        {
+            return "value";
+        }
+        
+
+
+        [HttpPost]
+        public IHttpActionResult CustomerInsert(CustomerModel cust)
+        {
+            CustomerDAL ds = new CustomerDAL();
+            CustomerBAL ba = new CustomerBAL();
+
+            ba.EmailId = cust.EmailId;
+            ba.Password = cust.Password;
+            ba.FirstName = cust.FirstName;
+            ba.LastName = cust.LastName;
+            ba.MobileNo = cust.MobileNo;
+            ba.City = cust.City;
+
+            string s1 = ds.AddCustomer(ba);
+            return Ok(new { status = 200, isSuccess = true, message = "ur logged sucess", });
+
+        }
+    }
+}
+
+
+
+
---
+++ LoginController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BalLibrary;
+using DalLibrary;
+using System.Web.Http.Cors;
+using ProjectWebApplication.Models;
+
+namespace ProjectWebApplication.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class LoginController : ApiController
+    {
+
+        public string Get()
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public IHttpActionResult AdminLogin(AdminModel ba)
+        {
+
+            AdminBAL ba1 = new AdminBAL();
+            ba1.AdminId = ba.AdminId;
+            ba1.AdminPassword = ba.AdminPassword;
+            LoginDAL ds = new LoginDAL();
+            bool ans = ds.ValidateAdmin(ba1);
+            if (ans == false)
+            {
+                return Ok(new { status = 401, isSuccess = false, message = "Invalid user", });
+
+            }
+            else
+            {
+                return Ok(new { status = 200, isSuccess = true, message = "valid user" });
+
+            }
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult CustomerLogin(CustomerLoginModel cust)
+        {
+
+            CustomerLoginBAL bal = new CustomerLoginBAL();
+            LoginDAL dal = new LoginDAL();
+
+            bal.EmailId = cust.EmailId;
+            bal.Password = cust.Password;
+            bool ans = dal.ValidateCustomer(bal);
+            if (ans == false)
+            {
+                return Ok(new { status = 401, isSuccess = false, message = "Invalid user", });
+            }
+            else
+            {
+                return Ok(new { status = 200, isSuccess = true, message = "valid user" });
+            }
+        }
+    }
+}
